14a1ebc4e8cc66fccf6c75d728eb0ecacc2195f4
Ashima <ashimagarg80@gmail.com>
2022-01-23 14:08:29 +0530

    commit message

---
+++ Hello world.py
@@ -0,0 +1,34 @@
+
+print("Hello world!!")
+a,b=1,1
+if(print("ifff")):
+    print("equal")
+
+
+"""
+
+a, b = 12, 5
+if a + b:
+    print('True')
+else:
+  print('False')
+
+
+if False:
+    print("Nissan")
+elif True:
+    print("Ford")
+elif True:
+    print("BMW")
+else:
+    print("Audi")
+
+if (1, 2):
+	print(' Learnpython4 ')
+
+a=1
+b=2
+if a==1 and b==2 and a!=b:
+    print("inside....")
+
+"""
