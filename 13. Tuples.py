7d83174db541a4a34814f381e297d09063fa577a
ashima80garg <54340909+ashima80garg@users.noreply.github.com>
2022-08-05 09:55:05 +0530

    Update 13. Tuples.py

--- 13. Tuples.py
+++ 13. Tuples.py
@@ -1,5 +1,4 @@
-if __name__ == '__main__':
-    n = int(input())
-    integer_list = map(int, input().split())
-    tup = tuple(integer_list)
-    print(hash(tup))
+n = int(input())
+integer_list = map(int, input().split())
+tup = tuple(integer_list)
+print(hash(tup))
