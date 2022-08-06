64b0efaafa89519bfdf22aecd9f556061db4eeda
ashima80garg <54340909+ashima80garg@users.noreply.github.com>
2022-08-06 12:36:36 +0530

    Update 2. PythonIfelse.py

--- 2. PythonIfelse.py
+++ 2. PythonIfelse.py
@@ -1,17 +1,5 @@
-import math
-import os
-import random
-import re
-import sys
-
-
-
-if __name__ == '__main__' :
-    n = int(input())
-    if n % 2 != 0:
-        print("Weird")
-    elif n >= 6 and n <=20:
-        print("Weird")
-    else:
-        print("Not Weird")
-    
+n = int(input())
+if n % 2 != 0 or n in range(6,21):
+    print("Weird")
+else:
+    print("Not Weird")
