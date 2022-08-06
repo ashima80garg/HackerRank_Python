a36fe7347222730f71a292b3f97e0a98a3c473de
ashima80garg <54340909+ashima80garg@users.noreply.github.com>
2022-08-06 12:47:05 +0530

    Update 10. NestedList.py

--- 10. NestedList.py
+++ 10. NestedList.py
@@ -1,7 +1,32 @@
-i = int(input())
-marksheet = [ [input(), float( input() )] for _ in range(i)]
-    
-scores = sorted(set([entry[1] for entry in marksheet]))
+n=int(input())
+scores=[]
+final_names=[]
 
-for name in sorted([entry[0] for entry in marksheet if entry[1] == scores[1]]):
-print(name)
+student=[[str(input()),float(input())]for i in range(n)]
+#print(student)
+#print(student[0])
+for i in range(n):
+    scores.append(student[i][1])
+#print(scores)
+
+scores.sort()
+#print(scores)
+
+lowest=scores[0]
+#print(lowest)
+
+for i in range(n):
+    if scores[i]>lowest:
+        sec_lowest=scores[i]
+        break
+#print(sec_lowest)
+
+for i in range(n):
+    if student[i][1]==sec_lowest:
+        final_names.append(student[i][0])
+
+final_names.sort()
+#print(final_names)
+for elements in final_names: # to print all individual elements
+    print(elements)
+
